ca92f45c80c310cc189fa5000ec66520137b0525
Evgenii Zamana <evgenii.zamana@gmail.com>
2021-03-19 03:43:28 +0300

    adding product block

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import './styles/style.scss';
 import 'slick-carousel'
 
 
-$('.switch-btn').on('click', () => {
-  $('.plus-btn__line').toggleClass('closed')
-  $('.products__container').toggleClass('hideBlock')
-})
-
 $(document).ready(function () {
   $('.brands__container').slick({
     /*speed: 300,
@@ -17,15 +12,42 @@ $(document).ready(function () {
 
       breakpoint: 1900,
       settings: {
-        slidesToShow:  3,
+        slidesToShow: 3,
         infinite: false
       }
     }]
   });
 })
 
+$('.switch-btn').on('click', () => {
+  $('.plus-btn__line').toggleClass('closed')
+  $('.products__container').toggleClass('hideBlock')
+})
 
-var htmlContent = $('.buy-btn').html();
-console.log(htmlContent);
+/*$('.product__btn').on('click', (event) => {
+  let htmlContent;
+  /!*htmlContent = $('.buy-btn').parent('.product-name').css("background", "yellow")*!/
+  /!*htmlContent = $(this);*!/
+  /!*console.log(htmlContent)*!/
+  htmlContent = $(event.target).closest('.test-block').children('.product-name').html()
+  console.log(htmlContent)
+})*/
+
+$('.product__cost__value').each(function () {
+  let costValues = $(this).html().split('')
+
+  costValues.forEach(value => {
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('product__cost__character')
+    newDiv.classList.add('character-digital')
+    newDiv.innerHTML = value;
+    $(this).parent().append(newDiv)
+  })
+})
+
+
+
+
+
 
 
